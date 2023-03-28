4bdaeedcc1e16e7154ceb276b2080fc5663a4013
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-03-28 02:41:31 +0000

    Added changes on lastChar task

--- string-manipulation-2/src/last-chars.js
+++ string-manipulation-2/src/last-chars.js
@@ -1,15 +1,27 @@
 /* exported lastChars */
+// function lastChars(length, string) {
+//   let newStr = '';
+//   if (string.length < length) {
+//     return string;
+//   } else {
+//     for (let i = string.length - 1; i >= length; i--) {
+//       newStr = string[i] + newStr;
+//       if (newStr.length === length) {
+//         break;
+//       }
+
+//     }
+//   }
+//   return newStr;
+// }
+
 function lastChars(length, string) {
   let newStr = '';
   if (string.length < length) {
     return string;
   } else {
-    for (let i = string.length - 1; i > length; i--) {
+    for (let i = string.length - 1; i >= string.length - length; i--) {
       newStr = string[i] + newStr;
-      if (newStr.length === length) {
-        break;
-      }
-
     }
   }
   return newStr;
