90d25de655f483affa43cffd76557cd22839fbd0
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-03 18:05:43 +0000

    Completed dom-quering exercise

---
+++ dom-querying/main.js
@@ -0,0 +1,16 @@
+
+const header1 = document.querySelector('h1');
+console.log(header1);
+console.dir(header1);
+const someId = document.querySelector('#explanation');
+console.log(someId);
+console.dir(someId);
+const someClass = document.querySelector('.hint');
+console.log(someClass);
+console.dir(someClass);
+const allPTags = document.querySelectorAll('p');
+console.log(allPTags);
+console.dir(allPTags);
+const allClass = document.querySelectorAll('.example-link');
+console.log(allClass);
+console.dir(allClass);
