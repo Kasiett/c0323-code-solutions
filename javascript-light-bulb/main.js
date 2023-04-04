b82c2094fd0b37235087cb042c01142e29ee1f2a
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-04 18:59:10 +0000

    Completed javascript-light-bulb practice

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,9 +1,15 @@
 const $container = document.querySelector('.container');
 const $bulb = document.querySelector('.bulb');
-console.log($bulb);
-console.log($container);
 
 $bulb.addEventListener('click', lightSwitch);
-function lightSwitch(event) {
 
+function lightSwitch(event) {
+  console.log('event = >', event);
+  if ($bulb.className === 'bulb' && $container.className === 'container') {
+    $bulb.className = 'bulb-off';
+    $container.className = 'container-off';
+  } else {
+    $bulb.className = 'bulb';
+    $container.className = 'container';
+  }
 }
