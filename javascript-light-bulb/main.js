b44694a20b6855ce993d1797caa54e174ae4f6d8
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-04 18:29:50 +0000

    in progress

--- javascript-light-bulb/main.js
+++ javascript-light-bulb/main.js
@@ -1,11 +1,9 @@
-let $container = document.querySelector('.container');
-let $bulb = document.querySelector('.bulb');
-// console.log($bulb);
+const $container = document.querySelector('.container');
+const $bulb = document.querySelector('.bulb');
+console.log($bulb);
+console.log($container);
 
 $bulb.addEventListener('click', lightSwitch);
-
 function lightSwitch(event) {
-  $container = 'container container - off';
-  $bulb = 'bulb bulb-off';
+
 }
-lightSwitch($container, $bulb);
