145ae9e0e2c08274fda49f94a4b70fa98763ae2e
Kasiet Rakhimdinova <k.rakhimdinova@gmail.com>
2023-04-05 03:29:08 +0000

    Deleted console.log

--- javascript-typing-tutor/main.js
+++ javascript-typing-tutor/main.js
@@ -1,7 +1,6 @@
 let index = 0;
 const currentChar = document.querySelectorAll('span');
 document.addEventListener('keydown', keyPressed);
-console.log(currentChar[index]);
 
 function keyPressed(event) {
   if (currentChar[index].textContent === event.key) {
